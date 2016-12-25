16233fdda701ea7802ad6c03c0dbd8426abf33a6
Shubham <2014UGEC078@nitjsr.ac.in>
2016-12-25 22:02:11 +0530

    Images Used Folder Added

--- app/src/main/java/com/seals/shubham/snapbook/MainActivity.java
+++ app/src/main/java/com/seals/shubham/snapbook/MainActivity.java
@@ -1,13 +1,24 @@
 package com.seals.shubham.snapbook;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Handler hnd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hnd = new Handler();
+        hnd.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(),Login.class);
+                MainActivity.this.startActivity(i);
+                MainActivity.this.finish();
+            }
+        },5000);
     }
 }
